afec42dde3d0d620d0b1ad1eee142e1cc4e46844
MrEremenko <illarion@gradcent.com>
2022-04-01 14:52:42 -0700

    font size changes

--- src/App.js
+++ src/App.js
@@ -7,32 +7,32 @@ import profile from './profile.jpeg';
 
 function App() {
   return (
-    <div className="App" style={{ color: "#0F2A54", display: "flex", flexDirection: "column", alignItems: "center" }}>
+    <div className="App" style={{ color: "#0F2A54", display: "flex", flexDirection: "column", alignItems: "center", fontFamily: "Times New Roman" }}>
       <div style={{ width: "100%", margin: "1vh 0vh", display: "flex", justifyContent: "space-around", flexWrap: "wrap" }}>
         <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-          <div style={{ fontSize: "2.5vh", textAlign: "center" }}>Elena Eremenko, World Language Proficiency Exam</div>
-          <div style={{ fontSize: "2vh", textAlign: "center" }}>and Russian Language Tutor</div>
+          <div style={{ fontSize: "2.8vh", textAlign: "center" }}>Elena Eremenko, World Language Proficiency Exam</div>
+          <div style={{ fontSize: "2.3vh", textAlign: "center" }}>and Russian Language Tutor</div>
         </div>
         <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
-          <div style={{ fontSize: "2.2vh", textAlign: "center" }}>ElenaRussianLanguage@gmail.com</div>
+          <div style={{ fontSize: "2.6vh", textAlign: "center" }}>ElenaRussianLanguage@gmail.com</div>
         </div>
       </div>
       <div style={{ backgroundImage: `url(${seattle})`, backgroundRepeat: "no-repeat", backgroundSize: "cover", color: "white", width: "100%", minHeight: "20vh",
         display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "space-around", flexWrap: "wrap" }}>
-          <div style={{ fontSize: "4vh", textAlign: "center" }}>Get your World Language Credits!</div>
+          <div style={{ fontSize: "4.3vh", textAlign: "center" }}>Get your World Language Credits!</div>
       </div>
-      <div style={{ fontSize: "5vh", textAlign: "center", marginTop: "1.5vh" }}>How can I help?</div>
-      <div style={{ margin: "2vh 10vw 0", boxShadow: "0 0px 1.2vh black", padding: "4vh", width: "70vw", display: "flex", flexDirection: "column", alignItems: "center"}}>
+      <div style={{ fontSize: "4vh", textAlign: "center", marginTop: "1.5vh" }}>How can I help?</div>
+      <div style={{ margin: "2vh 10vw 0", boxShadow: "0 0px 1.2vh black", padding: "4vh", width: "50vw", display: "flex", flexDirection: "column", alignItems: "center"}}>
         <div style={{ fontSize: "3.5vh", marginTop: "3vh", textAlign: "center", marginBottom: "1.5vh" }}>
           I can help you pass the <u>World Language Proficiency Test</u> <br/>
           and get <u>up to 4 credits</u> for High School, College or <br/>
           University.</div>
         <div style={{ fontSize: "3.5vh", textAlign: "center" }}></div>
       </div>
-      <div style={{ fontSize: "5vh", textAlign: "center", marginTop: "1.5vh" }}>How my lessons look like:</div>
-      <div style={{ margin: "2vh 10vw 0", boxShadow: "0 0px 1.2vh black", padding: "4vh", width: "70vw", display: "flex", flexDirection: "column", alignItems: "center"}}>
+      <div style={{ fontSize: "4vh", textAlign: "center", marginTop: "1.5vh" }}>How my lessons look like:</div>
+      <div style={{ margin: "2vh 10vw 0", boxShadow: "0 0px 1.2vh black", padding: "4vh", width: "60vw", display: "flex", flexDirection: "column", alignItems: "center"}}>
         <div style={{ fontSize: "3vh", margin: "3vh 1vw" }}>
-          <ul style={{ margin: "1vh 0 0 2vw" }}>
+          <ul style={{ margin: "1vh 0 0 0" }}>
             <li>Online lessons through Zoom.</li>
             <li>Both group and individual learning.</li>
             <li>Students ranging from Elementary and middle school students</li>
@@ -68,10 +68,10 @@ function App() {
         </div>
       </div>
       <hr/>
-      <div style={{ fontSize: "5vh", textAlign: "center", marginTop: "1.5vh" }}>My credentials</div>
-      <div style={{ margin: "2vh 10vw 0", boxShadow: "0 0px 1.2vh black", padding: "4vh", width: "70vw", display: "flex", flexDirection: "column", alignItems: "center"}}>
+      <div style={{ fontSize: "4vh", textAlign: "center", marginTop: "1.5vh" }}>My credentials</div>
+      <div style={{ margin: "2vh 10vw 0", boxShadow: "0 0px 1.2vh black", padding: "4vh", width: "60vw", display: "flex", flexDirection: "column", alignItems: "center"}}>
         <div style={{ fontSize: "3vh", margin: "3vh 1vw" }}>
-        <ul style={{ margin: "1vh 0 0 2vw" }}>
+        <ul style={{ margin: "1vh 0 0 0" }}>
             <li>17 years experience of teaching Russian heritage students.</li>
             <li>Russian Heritage Teacher Certificate from UW. “Preparing Russian <br/>Teacher for the 21st Century”</li>
             <li>Teacher Certificate for Russian as a Foreign Language from Moscow <br/>State University, Russia.</li>
@@ -84,11 +84,11 @@ function App() {
         </div>
       </div>
       <Container>
-      <div style={{ fontSize: "5vh", textAlign: "center", marginTop: "1.5vh" }}>Previous Experiences</div>
-        <Accordion>
+      <div style={{ fontSize: "4vh", textAlign: "center", marginTop: "2vh" }}>Previous Experiences</div>
+        <Accordion style={{ fontSize: "3vh" }}>
           <Accordion.Item eventKey="0">
-            <Accordion.Header>How my lessons look like.</Accordion.Header>
-            <Accordion.Body>
+            <Accordion.Header><div style={{ fontSize: "3vh" }}>How my lessons look like</div></Accordion.Header>
+            <Accordion.Body >
               For the last two years I am doing online lessons through the Zoom.
               I work with a group of students and individually.
               Elementary and middle school students in groups have two lessons
@@ -104,7 +104,7 @@ function App() {
             </Accordion.Body>
           </Accordion.Item>
           <Accordion.Item eventKey="1">
-            <Accordion.Header>My experience working with MSD</Accordion.Header>
+            <Accordion.Header><div style={{ fontSize: "3vh" }}>My experience working with MSD</div></Accordion.Header>
             <Accordion.Body>
               I started to work for the Mukilteo School District as a Natural Leader. I
               have 17 years experience working with Russian Ukrainian community
